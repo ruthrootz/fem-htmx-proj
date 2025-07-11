f9ffd5fa68dbf4e15ce747b297e9a7795af741e6
ruthrootz <rutholdja@icloud.com>
2025-07-10 20:44:42 -0400

    cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Co
 }
 
 func newTemplate() *Templates {
-  return &Templates{
+  return &Templates {
     templates: template.Must(template.ParseGlob("views/*.html")),
   }
 }
